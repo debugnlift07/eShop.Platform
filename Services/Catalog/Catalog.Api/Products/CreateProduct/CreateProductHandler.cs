58fd3ba748bc27607499895f90ccbb8212508124
Naveen Bisht <33859786+debugbyNB@users.noreply.github.com>
2026-01-02 11:54:23 +0530

    Catalog API- CQRS implemente with Add product

---
+++ Services/Catalog/Catalog.Api/Products/CreateProduct/CreateProductHandler.cs
@@ -0,0 +1,26 @@
+﻿
+namespace Catalog.Api.Products.CreateProduct
+{
+    public record CreateProductCommand(string Name, List<String> Category, string Description, string ImageFile, decimal Price)
+        : ICommand<CreateProductResult>;
+    public record CreateProductResult(Guid Id);
+    public class CreateProductCommandHandler
+        : ICommandHandler<CreateProductCommand, CreateProductResult>
+    {
+        public async Task<CreateProductResult> Handle(CreateProductCommand command, CancellationToken cancellationToken)
+        {
+            //create product entity using cammand
+            var product = new Product
+            {
+                Name = command.Name,
+                Category = command.Category,
+                Description = command.Description,
+                ImageFile = command.ImageFile,
+                Price = command.Price,
+            };
+            //save to database
+            //return result
+            return new CreateProductResult(Guid.NewGuid());
+        }
+    }
+}
--- Services/Catalog/Catalog.Api/Program.cs
+++ Services/Catalog/Catalog.Api/Program.cs
@@ -1,6 +1,16 @@
+using Catalog.Api.Products.CreateProduct;
+
 var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddCarter();
+
+builder.Services.AddMediatR(config =>
+{
+    config.RegisterServicesFromAssemblyContaining<CreateProductCommand>();
+});
+
 var app = builder.Build();
 
-app.MapGet("/", () => "Hello World!");
+app.MapCarter();
 
 app.Run();
