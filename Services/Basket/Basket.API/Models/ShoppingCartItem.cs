1af7ebe9b1cb0f885ae167828f00c956cfe1a9a4
Naveen Bisht <33859786+debugbyNB@users.noreply.github.com>
2026-01-07 19:22:01 +0530

    Basket Api: Setup Created.

---
+++ Services/Basket/Basket.API/Models/ShoppingCartItem.cs
@@ -0,0 +1,11 @@
+﻿namespace Basket.API.Models
+{
+    public class ShoppingCartItem
+    {
+        public int Quantity { get; set; } = default!;
+        public int Color { get; set; } = default!;
+        public int Price { get; set; } = default!;
+        public int ProductId { get; set; } = default!;
+        public int ProductName { get; set; } = default!;
+    }
+}
